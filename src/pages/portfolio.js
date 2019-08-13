a5b1744bf29ebb1f2ab32c663cb77a9f73e1889d
Ben Jenkins <ben.jenkins@icloud.com>
2019-08-12 20:58:47 -0500

    add screenshots

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,8 +1,8 @@
 /* eslint-disable prettier */
 
 import React from "react"
-import { Link } from "gatsby"
-// import Header from "../components/header"
+import reactfx from '../images/reactfx.png'
+import gracefulShoveler from '../images/gracefulShoveler.png'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -11,16 +11,18 @@ const Portfolio = () => (
     <SEO title="Portfolio" />
     <h2>React FX - Sole Developer</h2>
     <h5>Realtime audio processing emulating electric guitar effects.</h5>
+    <img src={reactfx} alt="App screenshot" />
     <p>
       React FX uses React, ToneJS, and Styled Components to replicate the
-      experience of using electric guitar effects pedals. I only used
-      function components so that I could learn Hooks to manage state and
-      implement a custom data layer modeled after Redux.
+      experience of using electric guitar effects pedals. I only used function
+      components so that I could learn Hooks to manage state and implement a
+      custom data layer modeled after Redux.
     </p>
     <h2>Graceful Shoveler - Developer</h2>
     <h5>
       A fully functional e-commerce website selling all varieties of shovels.
     </h5>
+    <img src={gracefulShoveler} alt="App screenshot" />
     <p>
       This project was completed by a team of four in nine days as part of the
       Grace Shopper project during our time at Fullstack Academy. My
