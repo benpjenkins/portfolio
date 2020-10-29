2989760ce68795574d621012bcf3e626b5ebc707
Ben Jenkins <ben.jenkins@icloud.com>
2020-10-29 18:37:45 -0500

    intitial refresh commit

---
+++ gatsby/gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `Ben Jenkins`,
+    description: `The portfolio and blog of Ben Jenkins, a fullstack software engineer from Chicago.`,
+    author: `Ben Jenkins`,
+  }
+}
